a33d7db2c696e6f4380999ae28826ad152e7d32a
ayelemintesnot732-bit <ayelemintesnot732@gmail.com>
2026-05-08 12:17:32 +0300

    Delete metro.config.js

--- metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Add support for additional file types
-config.resolver.assetExts.push('cjs', 'bin', 'pdf');
-config.resolver.sourceExts.push('js', 'jsx', 'json', 'ts', 'tsx');
-
-module.exports = config;
